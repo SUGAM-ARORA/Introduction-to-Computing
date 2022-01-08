96fddac5aafc1c6e5776c7de541ccc59aba0b6c6
SUGAM ARORA <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-01-08 22:57:00 +0530

    Add files via upload

---
+++ asg1_21105021 output.py
@@ -0,0 +1,32 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/SUGAM ARORA/Desktop/asg1_21105021.py ============
+ASSIGNMENT 1
+ 1. Write a Python program to find average of three numbers entered by the user.
+Enter first number : 70
+Enter second number : 50
+Enter third number : 80
+Average :  66.66666666666667
+ 2. Write a python program to compute a person's income tax. 
+Enter the Gross Income to the nearest penny : 300000
+Enter the number of Dependents : 8
+53200.0
+3. Write a python program to store different data type values into a list.
+Enter your SID : 21105021
+Enter your name : Sugam Arora
+Enter your Gender-(M,F,U) :  M
+Enter the course name : ece
+Enter the CGPA : 10
+[21105021, 'Sugam Arora', 'M', 'ece', 10.0]
+4. Write a python program to enter marks of 5 students into a list and display them in sorted manner.
+Enter the marks in subject 1 = 86
+Enter the marks in subject 2 = 91
+Enter the marks in subject 3 = 68
+Enter the marks in subject 4 = 77
+Enter the marks in subject 5 = 82
+[68.0, 77.0, 82.0, 86.0, 91.0]
+5. a) List: color ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+5. b) List: color ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
