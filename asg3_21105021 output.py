b0d4407e8ea06916ffa0c073ea1a4bc7693a53a7
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-02-06 12:21:33 +0530

    Add files via upload

---
+++ asg3_21105021 output.py
@@ -0,0 +1,112 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+
+Question 1
+Enter the string: hello my name is sugam arora and i am from ECE
+
+Dictionary containing {'Word':'Word Count'} is shown below:
+{'arora': 1, 'from': 1, 'and': 1, 'ece': 1, 'my': 1, 'i': 1, 'sugam': 1, 'is': 1, 'name': 1, 'hello': 1, 'am': 1}
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+
+Question 2
+Enter Date below to get date of next day.
+
+Enter Day [1-31]:22
+Enter Month [1-12]:7
+Enter Year [1800-2025]:2008
+
+Next Date in format Day/Month/Year is: 23/7/2008
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+
+Question 3
+Enter the numbers separated by space:1 2 3 4 5
+
+List containing (n,n^2) is shown below:
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+
+Question 4
+Enter grade points:10
+Your grade is 'A+' and Outstanding performance.
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+
+Question 5
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+Enter student name:Sugam Arora
+Enter student SID:21105021
+Enter Y to continue or N to end:Y
+Enter student name:Vidit Sarin
+Enter student SID:21105035
+Enter Y to continue or N to end:N
+
+Q.6(a)
+The Dictionary containing {'SID':'Name'} is shown below
+{21105021: 'Sugam Arora', 21105035: 'Vidit Sarin'}
+
+Q.6(b)
+The Dictionary after sorting according to name:
+{21105021: 'Sugam Arora', 21105035: 'Vidit Sarin'}
+
+Q.6(c)
+The Dictionary after sorting according to SID:
+{21105021: 'Sugam Arora', 21105035: 'Vidit Sarin'}
+
+Q.6(d)
+Enter SID to get name of student:21105021
+Name of student with SID 21105021 is Sugam Arora.
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+
+Question 7
+Enter number of elements N in fibonacci series:
+[N must be positive Integer]: N=10
+
+The fibonacci series with 10 elements is shown below:
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+Average of given fibonacci series is 14.30
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+
+Question 7
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+Q.8(a)
+
+A new Set of all 'elements that are in Set1 and Set2 but not both' is:
+{1, 3, 5, 6, 8}
+
+Q.8(b)
+
+A new set of all elements that are 'in only one of the three sets Set1,Set2 and Set3' is:
+{3, 6, 8, 9, 13, 17}
+
+Q.8(c)
+
+A new set of elements that are 'exactly in two of the sets Set1, Set2 and Set3' is:
+{1, 2, 4, 5}
+
+Q.8(d)
+
+A new set of all Integers in the 'range 1 to 10' that are 'not in Set1' is:
+{6, 7, 8, 9, 10}
+
+Q.8(e)
+
+A new set of all Integers in the range 1 to 10 that are not in Set1,Set2 and Set3.
+{10, 7}
