ef873b9a130fd64ac5ff9f9ec57932c0e3546079
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-02-25 16:50:02 +0530

    Add files via upload

---
+++ asg4_21105021 output.py
@@ -0,0 +1,109 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+ASSIGNMENT 4
+
+ Q1Write a recursive python function to solve the problem of tower of Hanoi with three disks.
+
+Enter number of disk in tower of Hanoi:4
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Auxiliary
+Move disk 2 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+Move disk 3 from Source Tower to Auxiliary
+Move disk 1 from Destination to Source Tower
+Move disk 2 from Destination to Auxiliary
+Move disk 1 from Source Tower to Auxiliary
+Move disk 4 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+Move disk 2 from Auxiliary to Source Tower
+Move disk 1 from Destination to Source Tower
+Move disk 3 from Auxiliary to Destination
+Move disk 1 from Source Tower to Auxiliary
+Move disk 2 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+
+================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ==================
+
+ Q2 Write a python program to print the Pascal’s triangle for n number of rows given by the user using both recursive and iterative procedures (for/while loop).
+Enter number of rows:8
+                                            1                                             
+                                           1  1                                           
+                                         1  2  1                                          
+                                        1  3  3  1                                        
+                                      1  4  6  4  1                                       
+                                    1  5  10  10  5  1                                    
+                                  1  6  15  20  15  6  1                                  
+                                1  7  21  35  35  21  7  1                                
+
+====================================================================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ======================================================================
+
+ Q3 Input two integer values from user, calculate and print the quotient and reminder obtained from the two values
+
+Enter an Integer:1024
+Enter another Integer:15
+
+The quotient when 1024 is divided by 15 is 68.
+The remainder when 1024 is divided by 15 is 4.
+
+Que3.a
+Function is callable
+1024 is Not-callable
+15 is Not-callable
+
+Que3.b
+All result values are 'non-zero'
+
+Que3.c
+Values greater than 4 is 68,5,6
+
+Que3.d
+Above result in set form is shown below:
+{68, 5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 68
+Hash value of 68 is 68
+
+====================================================================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ======================================================================
+
+ Q4 Create a class named Student, use the_init_() function to assign values for name and roll number. And also call _del_() function to destroy object that is created.
+
+Hello, My name is SUGAM ARORA and my roll no. is 21105021
+Destructor Called
+
+====================================================================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ======================================================================
+
+ Q5 Write a program to store details of three employees: name and salary using class.
+
+Employee Name is Mehak and Salary is 40000 
+Employee Name is Ashok and Salary is 50000 
+Employee Name is Viren and Salary is 60000 
+
+Que.5a
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000 
+
+Que.5b
+Employee Viren's data has been removed.
+
+====================================================================== RESTART: C:/Users/SUGAM ARORA/Desktop/1.py ======================================================================
+
+ Q6 Barbie and George are the two friends. On Saturday, they decided to travel to a fair where they discovered a fun game that put their friendship to the test. The test required George to utter a word and Barbie to create a new meaningful word using the exact same letters as George. If Barbie fails to form a word then their friendship is a fake.Can you assist the shopkeeper by writing a piece of code for him to use so that the test runs smoothly?
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:George
+Enter Player2 name:Barbie
+
+Enter Word by George:flow
+Enter Word by Barbie:wolf
+
+Friendship of George and Barbie is REAL
